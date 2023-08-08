6b8d22a06e9b5d09621ec2d20c20b49cdb936ee0
shail2401 <129515844+shail2401@users.noreply.github.com>
2023-08-09 00:10:01 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const WebSocket = require('ws');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('WebSocket server running');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('A user connected');
+
+    ws.on('message', (message) => {
+        console.log('Message received:', message);
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
